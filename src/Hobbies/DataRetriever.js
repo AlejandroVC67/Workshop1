480a85ed8bf127f996bf9efb11713cf03d3027b6
AlejandroVC67 <vcalejandro97@gmail.com>
2018-01-30 17:17:30 -0500

    Deleted some code of the DataRetriever.js

--- src/Hobbies/DataRetriever.js
+++ src/Hobbies/DataRetriever.js
@@ -1,12 +1,5 @@
 export default class DataRetriever {
     static get (url, callback) {
-      // var oReq = new XMLHttpRequest();
-      // oReq.addEventListener('load', (response) => {
-      //   callback(response.currentTarget.response)
-      // });
-      // oReq.responseType = 'json'
-      // oReq.open('GET', url);
-      // oReq.send();
       fetch(url)
         .then((response) => {
           return response.json()
